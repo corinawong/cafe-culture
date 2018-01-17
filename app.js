dffce82337772b08034bb4cb9bf5a65a24e45adc
Corina Wong <hello@corinawong.com>
2018-01-17 10:05:04 -0500

    Initial styles with Bootstrap

--- app.js
+++ app.js
@@ -4,14 +4,36 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 var cafes = [
-  { name: "Cat Café" },
-  { name: "Board Games Café" },
-  { name: "Robot Café" }
+  {
+    name: "Cat Café",
+    image: "https://farm2.staticflickr.com/1689/24853484936_04d64c273a_k.jpg"
+  },
+  {
+    name: "Board Games Café",
+    image: "https://farm5.staticflickr.com/4110/5024733655_892e48ed8c_b.jpg"
+  },
+  {
+    name: "Robot Café",
+    image: "https://farm2.staticflickr.com/1401/1475297269_3082a71c04_z.jpg"
+  },
+  {
+    name: "Cat Café",
+    image: "https://farm2.staticflickr.com/1689/24853484936_04d64c273a_k.jpg"
+  },
+  {
+    name: "Board Games Café",
+    image: "https://farm5.staticflickr.com/4110/5024733655_892e48ed8c_b.jpg"
+  },
+  {
+    name: "Robot Café",
+    image: "https://farm2.staticflickr.com/1401/1475297269_3082a71c04_z.jpg"
+  }
 ];
 
 app.get("/", (req, res) => {
