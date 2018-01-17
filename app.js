a74ced009723da016c2a6f2b30dba12d69885a8b
Corina Wong <hello@corinawong.com>
2018-01-17 14:03:24 -0500

    Add Mongo/Mongoose

--- app.js
+++ app.js
@@ -2,39 +2,38 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/cafe-culture", {
+  useMongoClient: true
+});
+
+// Schema setup
+const cafeSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+const Cafe = mongoose.model("Cafe", cafeSchema);
+
+// Cafe.create({
+//   name: "Board Games Café",
+//   image: "https://farm5.staticflickr.com/4110/5024733655_892e48ed8c_b.jpg"
+// }).then(
+//   cafe => {
+//     console.log("NEW CAFE", cafe);
+//   },
+//   err => {
+//     console.log(err);
+//   }
+// );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
-var cafes = [
-  {
-    name: "Cat Café",
-    image: "https://farm2.staticflickr.com/1689/24853484936_04d64c273a_k.jpg"
-  },
-  {
-    name: "Board Games Café",
-    image: "https://farm5.staticflickr.com/4110/5024733655_892e48ed8c_b.jpg"
-  },
-  {
-    name: "Robot Café",
-    image: "https://farm2.staticflickr.com/1401/1475297269_3082a71c04_z.jpg"
-  },
-  {
-    name: "Cat Café",
-    image: "https://farm2.staticflickr.com/1689/24853484936_04d64c273a_k.jpg"
-  },
-  {
-    name: "Board Games Café",
-    image: "https://farm5.staticflickr.com/4110/5024733655_892e48ed8c_b.jpg"
-  },
-  {
-    name: "Robot Café",
-    image: "https://farm2.staticflickr.com/1401/1475297269_3082a71c04_z.jpg"
-  }
-];
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -43,10 +42,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/cafes", (req, res) => {
-  res.render("cafes", {
-    title: "Cafés",
-    cafes
-  });
+  Cafe.find().then(
+    cafes => {
+      res.render("cafes", {
+        title: "Cafés",
+        cafes
+      });
+    },
+    err => {
+      console.log(err);
+    }
+  );
 });
 
 app.get("/cafes/new", (req, res) => {
@@ -56,10 +62,17 @@ app.get("/cafes/new", (req, res) => {
 });
 
 app.post("/cafes", (req, res) => {
-  let name = req.body.name;
-  let newCafe = { name };
-  cafes.push(newCafe);
-  res.redirect("/cafes");
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCafe = { name, image };
+  Cafe.create(newCafe).then(
+    cafe => {
+      res.redirect("/cafes");
+    },
+    e => {
+      console.log(e);
+    }
+  );
 });
 
 app.listen(3000, () => {
